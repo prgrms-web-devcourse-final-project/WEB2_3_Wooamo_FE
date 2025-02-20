b4c800f8b7e1cee1b02d22d37b6ec12383a3d047
ddongguri-bing <ckdalssnsk@naver.com>
2025-02-20 14:51:05 +0900

    💄 채팅 메인 페이지에 친구 채팅 목록 추가

--- src/app/(with-main-header)/chatting/page.tsx
+++ src/app/(with-main-header)/chatting/page.tsx
@@ -1,3 +1,35 @@
+import ChattingHeader from "./ChattingHeader";
+import ChattingItem from "./ChattingItem";
+
 export default function Chatting() {
-  return <h1>Chatting</h1>;
+  return (
+    <div className="flex flex-col gap-13">
+      <ChattingHeader />
+      <section className="flex flex-col">
+        {new Array(5)
+          .fill(0)
+          .map((_, idx) =>
+            idx % 2 === 0 ? (
+              <ChattingItem
+                key={idx}
+                id={idx}
+                nickname={`user ${idx}`}
+                lastChatContent="혹시 자료 공유 해주실 수 있나요?"
+                read={true}
+                chatTimestamp="방금"
+              />
+            ) : (
+              <ChattingItem
+                key={idx}
+                id={idx}
+                nickname={`user ${idx}`}
+                lastChatContent="반가워요!"
+                read={false}
+                chatTimestamp="3시간 전"
+              />
+            )
+          )}
+      </section>
+    </div>
+  );
 }
