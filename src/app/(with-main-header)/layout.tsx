6cb6980f213af4c5c2639d2b32ef5b2a07b9359c
ddongguri-bing <ckdalssnsk@naver.com>
2025-02-20 17:15:07 +0900

    💄 메인 레이아웃 배경 수정

--- src/app/(with-main-header)/layout.tsx
+++ src/app/(with-main-header)/layout.tsx
@@ -5,7 +5,7 @@ import Noise from "@/components/common/Noise";
 export default function layout({ children }: { children: ReactNode }) {
   return (
     <Noise>
-      <div className="w-full min-h-screen h-full bg-site-bg overflow-x-hidden">
+      <div className="w-full min-h-screen h-screen bg-site-bg overflow-x-hidden">
         <Header />
         <main className="px-12 pb-13 pt-40">{children}</main>
       </div>
