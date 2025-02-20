8c265737ac253043807e6bf61d427fd0398581d6
ddongguri-bing <ckdalssnsk@naver.com>
2025-02-20 14:48:10 +0900

    💄 채팅 목록 아이템 컴포넌트 추가

---
+++ src/app/(with-main-header)/chatting/ChattingItem.tsx
@@ -0,0 +1,44 @@
+import Avatar from "@/components/common/Avatar";
+import basic from "@/assets/images/costumes/basic.png";
+import Link from "next/link";
+
+interface ChatItemProps {
+  id: number;
+  nickname: string;
+  lastChatContent: string;
+  read: boolean;
+  chatTimestamp: string;
+}
+
+export default function ChattingItem({
+  id,
+  nickname,
+  lastChatContent,
+  read,
+  chatTimestamp,
+}: ChatItemProps) {
+  return (
+    <Link href={`/chatting/friend/${id}`}>
+      <article
+        className={`h-25 px-8 flex justify-between items-center ${
+          read ? "bg-site-white-50" : ""
+        }`}
+      >
+        <div className="flex items-center gap-2.5">
+          <div>
+            <Avatar className="w-15 h-15" costumeSrc={basic} />
+          </div>
+          <div className="flex flex-col gap-1">
+            <div>
+              <span className="font-semibold text-xl">{nickname}</span>
+            </div>
+            <div className="flex gap-3">
+              <span className="text-site-darkgray-02">{lastChatContent}</span>
+              <span className="text-site-darkgray-01">{chatTimestamp}</span>
+            </div>
+          </div>
+        </div>
+      </article>
+    </Link>
+  );
+}
