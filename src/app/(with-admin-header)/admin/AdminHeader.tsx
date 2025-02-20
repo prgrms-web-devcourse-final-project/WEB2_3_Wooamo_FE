46ffb743132a47c1b1b2805485c5898788bd9658
ddongguri-bing <ckdalssnsk@naver.com>
2025-02-21 01:04:43 +0900

    💄 관리자 페이지 헤더 추가

---
+++ src/app/(with-admin-header)/admin/AdminHeader.tsx
@@ -0,0 +1,21 @@
+import Logo from "@/assets/images/Logo.svg";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function AdminHeader() {
+  return (
+    <div className="fixed z-10 w-full px-15 h-25 bg-site-button flex justify-between items-center">
+      <div className="">
+        <div className="flex gap-5 items-center">
+          <Link href={"/admin"}>
+            <Image src={Logo} alt={`STUV 로고`} />
+          </Link>
+        </div>
+      </div>
+      <div className="flex gap-5 font-semibold">
+        <div className="px-4 py-3 bg-site-white-70 rounded-lg">Grapana</div>
+        <div className="px-4 py-3 bg-site-white-70 rounded-lg">Logout</div>
+      </div>
+    </div>
+  );
+}
