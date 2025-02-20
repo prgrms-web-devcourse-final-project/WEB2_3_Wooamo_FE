2fff615a1230d6117a458ba228c9b9163b6968e3
ddongguri-bing <ckdalssnsk@naver.com>
2025-02-21 01:05:50 +0900

    💄 관리자 메인 페이지 추가 및 컴포넌트 생성

---
+++ src/app/(with-admin-header)/admin/PaymentList.tsx
@@ -0,0 +1,10 @@
+export default function PaymentList() {
+  return (
+    <div className="flex px-5 py-10 bg-site-white-70">
+      <div className="flex-3">2025-00-00 hh:mm:ss</div>
+      <div className="flex-1">홍길동</div>
+      <div className="flex-1">1,000</div>
+      <div className="flex-1">5,500원</div>
+    </div>
+  );
+}
