9e67065a2659a4b4374b1e39506f58f66a2741d9
ddongguri-bing <ckdalssnsk@naver.com>
2025-02-21 09:49:05 +0900

    💄 관리자 페이지 결제 목록 props 추가

--- src/app/(with-admin-header)/admin/PaymentList.tsx
+++ src/app/(with-admin-header)/admin/PaymentList.tsx
@@ -1,10 +1,22 @@
-export default function PaymentList() {
+interface PaymentListProps {
+  paymentTime: string;
+  name: string;
+  point: string;
+  value: string;
+}
+
+export default function PaymentList({
+  paymentTime,
+  name,
+  point,
+  value,
+}: PaymentListProps) {
   return (
     <div className="flex px-5 py-10 bg-site-white-70">
-      <div className="flex-3">2025-00-00 hh:mm:ss</div>
-      <div className="flex-1">홍길동</div>
-      <div className="flex-1">1,000</div>
-      <div className="flex-1">5,500원</div>
+      <div className="flex-3">{paymentTime}</div>
+      <div className="flex-1">{name}</div>
+      <div className="flex-1">{point}</div>
+      <div className="flex-1">{value}</div>
     </div>
   );
 }
