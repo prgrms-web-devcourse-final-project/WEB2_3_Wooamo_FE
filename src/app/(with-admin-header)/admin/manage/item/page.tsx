329e05fa26cb7240089f40010f510ae18128ccb4
ddongguri-bing <ckdalssnsk@naver.com>
2025-02-21 14:35:31 +0900

    💄 관리자 페이지 아이템 페이지 퍼블리싱에 불필요한 코드 삭제

--- src/app/(with-admin-header)/admin/manage/item/page.tsx
+++ src/app/(with-admin-header)/admin/manage/item/page.tsx
@@ -4,19 +4,17 @@ import AddCustume from "./AddCustume";
 
 export default function page() {
   return (
-    <>
-      <div className="flex flex-col gap-15">
-        <AddCustume />
-        <div className="flex flex-wrap gap-10">
-          {costumes.map((costume, index) => (
-            <CostumeItem
-              key={`costume${index}`}
-              index={index}
-              costume={costume}
-            />
-          ))}
-        </div>
+    <div className="flex flex-col gap-15">
+      <AddCustume />
+      <div className="flex flex-wrap gap-10">
+        {costumes.map((costume, index) => (
+          <CostumeItem
+            key={`costume${index}`}
+            index={index}
+            costume={costume}
+          />
+        ))}
       </div>
-    </>
+    </div>
   );
 }
