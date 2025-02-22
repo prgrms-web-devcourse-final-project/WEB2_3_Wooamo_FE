b63293ff2e0a11b30b13a46b6f8dc0cac1d2bac4
ddongguri-bing <ckdalssnsk@naver.com>
2025-02-22 23:29:12 +0900

    🔧 next.config.ts 이미지 리모트 패턴 추가

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+        port: "",
+        pathname: "/DevPumpkin0113/test/refs/heads/main/costume/**",
+        search: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
