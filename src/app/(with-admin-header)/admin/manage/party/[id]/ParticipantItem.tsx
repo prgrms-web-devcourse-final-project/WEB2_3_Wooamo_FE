353fe903a89578c0e87e7b23a4fe94afe3060808
ddongguri-bing <ckdalssnsk@naver.com>
2025-02-23 21:58:49 +0900

    ✨ 🔥 불필요한 props 삭제 및 이미지 태그에 너비와 높이 추가

--- src/app/(with-admin-header)/admin/manage/party/[id]/ParticipantItem.tsx
+++ src/app/(with-admin-header)/admin/manage/party/[id]/ParticipantItem.tsx
@@ -1,23 +1,23 @@
-import OrangeTurtle from "@/assets/images/costumes/orangeTurtle.png";
 import Image from "next/image";
 
 interface ParticipantItemProps {
   profile: string;
   nickname: string;
-  auth: string;
-  isAuth: string;
 }
 
 export default function ParticipantItem({
   profile,
   nickname,
-  auth,
-  isAuth,
 }: ParticipantItemProps) {
   return (
     <div className="w-40 px-5 py-5 bg-site-white-50 rounded-sm flex flex-col gap-4 items-center">
       <div className="w-18 h-18 bg-site-white-100 rounded-full">
-        <Image src={profile} alt="사용자 아바타 이미지" />
+        <Image
+          src={profile}
+          width={72}
+          height={72}
+          alt="사용자 아바타 이미지"
+        />
       </div>
       <div className="text-xs font-galmuri line-clamp-2">@{nickname}</div>
     </div>
