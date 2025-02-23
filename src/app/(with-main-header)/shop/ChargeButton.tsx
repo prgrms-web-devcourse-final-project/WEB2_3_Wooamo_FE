39504bbbe443e8f4ca078071475431f26c3cfeb6
KimSoohyun <tina971007@naver.com>
2025-02-23 20:48:05 +0900

    [STYLE] 숍 페이지 반응형 구현 (#33)
    * :lipstick: Style: 마이페이지 반응형 추가
    
    * :lipstick: Style: 숍페이지 반응형 구현현
    
    * :lipstick: Style: shopCostume 패딩 추가 및 파일명 오타 수정

--- src/app/(with-main-header)/shop/ChargeButton.tsx
+++ src/app/(with-main-header)/shop/ChargeButton.tsx
@@ -15,20 +15,22 @@ export default function ChargeButton() {
 
   return (
     <>
-      <Button onClick={() => open("charge")} className="absolute -right-6">
+      <Button onClick={() => open("charge")} className="relative -right-6">
         충전
       </Button>
 
       <Modal modalId="charge">
         <div className="flex flex-col items-center w-full gap-8">
-          <span className="font-semibold text-xl">포인트 숍</span>
+          <span className="font-semibold text-base lg:text-xl">포인트 숍</span>
           <div className="flex flex-col w-full gap-4">
             {payments.map((payment) => (
               <div
                 key={`${payment.point}p`}
                 className="flex justify-between items-center w-full px-8"
               >
-                <p className="font-semibold text-xl">{payment.point}p</p>
+                <p className="font-semibold text-base lg:text-xl">
+                  {payment.point}p
+                </p>
                 <Button>{payment.price.toLocaleString("ko-KR")}원</Button>
               </div>
             ))}
