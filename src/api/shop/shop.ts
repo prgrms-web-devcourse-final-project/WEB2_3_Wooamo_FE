b3f61d642f05b9423f78a24c6096bd2fbb9854e4
ddongguri-bing <ckdalssnsk@naver.com>
2025-02-25 00:27:59 +0900

    ✨ revalidate tag 이름 추가

--- src/api/shop/shop.ts
+++ src/api/shop/shop.ts
@@ -2,6 +2,7 @@ const getCostumeList = async (page: number) => {
   try {
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_SERVER_URL}/costume?page=${page}&size=10`,
+      { next: { tags: ["costume-list"] } },
     );
     if (!response.ok) throw new Error(response.statusText);
     const data: getCostumeListRes = await response.json();
