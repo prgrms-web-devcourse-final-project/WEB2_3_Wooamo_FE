cb40def6c23fcdc206a850e41121f2f81d63f6a0
ddongguri-bing <ckdalssnsk@naver.com>
2025-02-25 10:08:42 +0900

    ✨ 아이템 수정 기능 추가

--- src/app/(with-admin-header)/admin/manage/item/CostumeItem.tsx
+++ src/app/(with-admin-header)/admin/manage/item/CostumeItem.tsx
@@ -6,6 +6,7 @@ import Button from "@/components/common/Button";
 import Modal from "@/components/common/Modal";
 import { useModalStore } from "@/store/modalStore";
 import Image from "next/image";
+import { useState } from "react";
 
 interface ShopCostumeItemProps {
   costume: string;
@@ -21,11 +22,25 @@ export default function CostumeItem({
   point,
 }: ShopCostumeItemProps) {
   const { open, close } = useModalStore((state) => state);
+  const [itemName, setItemName] = useState<string>(name);
+  const [itemPoint, setItemPoint] = useState<number>(point);
 
   const handleCostumeDelete = async (costumeId: number) => {
     const response = await adminApi.deleteCostume(costumeId);
 
     if (response?.status === "성공") {
+      close();
+      revalidateTagAction("costume-list");
+    }
+  };
+
+  const handleCostumeEdit = async (costumeId: number) => {
+    const request = await adminApi.putCostumeEdit(costumeId, {
+      costumeName: itemName,
+      point: itemPoint,
+    });
+
+    if (request?.status === "성공") {
       close();
       revalidateTagAction("costume-list");
     }
@@ -38,7 +53,7 @@ export default function CostumeItem({
         onClick={() => open(`custuem-item${costumeId}`)}
       >
         <div className="flex justify-center items-center absolute -top-7.5 right-0 w-fit px-6 h-15 bg-site-sub rounded-full">
-          <span className="font-galmuri text-xl">300p</span>
+          <span className="font-galmuri text-xl">{point}p</span>
         </div>
         <Image
           src={costume}
@@ -49,7 +64,12 @@ export default function CostumeItem({
       </article>
 
       <Modal modalId={`custuem-item${costumeId}`} className="w-150">
-        <div className="flex flex-col gap-5">
+        <form
+          className="flex flex-col gap-5"
+          onSubmit={() => {
+            handleCostumeEdit(costumeId);
+          }}
+        >
           <div className="text-xl font-semibold">아이템 수정/삭제</div>
           <div className="flex gap-5">
             <div className="w-60 h-60 bg-site-button-input rounded-xl flex justify-center items-center">
@@ -66,38 +86,41 @@ export default function CostumeItem({
                 <div className="flex flex-col gap-3">
                   <label htmlFor="item-name">이름</label>
                   <input
-                    value={name}
+                    value={itemName}
                     id="item-name"
                     type="text"
                     placeholder="아이템 이름을 입력해주세요"
                     className="w-full px-5 py-2 border border-site-lightgray rounded-full"
-                    onChange={() => {}}
+                    onChange={(e) => setItemName(e.target.value)}
                   />
                 </div>
                 <div className="flex flex-col gap-3">
                   <label htmlFor="item-point">포인트</label>
                   <input
-                    value={point}
+                    value={itemPoint}
                     id="item-point"
-                    type="text"
+                    type="number"
                     placeholder="아이템 포인트를 입력해주세요"
                     className="w-full px-5 py-2 border border-site-lightgray rounded-full"
-                    onChange={() => {}}
+                    onChange={(e) => setItemPoint(Number(e.target.value))}
                   />
                 </div>
               </div>
               <div className="flex gap-5">
                 <Button
+                  type="button"
                   className="lg:h-11 lg:text-base"
                   onClick={() => handleCostumeDelete(costumeId)}
                 >
                   삭제하기
                 </Button>
-                <Button className="lg:h-11 lg:text-base">등록하기</Button>
+                <Button type="submit" className="lg:h-11 lg:text-base">
+                  수정하기
+                </Button>
               </div>
             </div>
           </div>
-        </div>
+        </form>
       </Modal>
     </>
   );
