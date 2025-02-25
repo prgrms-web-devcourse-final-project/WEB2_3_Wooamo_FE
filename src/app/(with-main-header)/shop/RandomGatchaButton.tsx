ac301e0131fab67b3bc1a3642152992da87075aa
ddongguri-bing <137764022+ddongguri-bing@users.noreply.github.com>
2025-02-26 00:46:58 +0900

    [FEAT] 상점 페이지 (#55)
    * ✨ 아이템 구매 API 함수 추가
    
    * ✨ 코스튬 목록과 현재 로그인된 사용자의 포인트 데이터 패칭
    
    * ✨ 상점 아이템 구매 기능 추가
    
    * ✨ 코스튬 랜덤 구매 API 함수 추가
    
    * 🏷️  랜덤 코스튬 구매 타입 추가 및 지정
    
    * ✨ 코스튬 랜덤 구매 기능 추가
    
    * ✨ 포인트 결제 요청 API 함수 및 타입 추가
    
    * ✨ 포인트 결제 검증 API 함수 추가
    
    * 📦 토스페이먼츠 결제위젯 SDK 설치
    
    * ✨ revalidateTag 추가
    
    * 🏷️ 토스 client key env 타입 추가
    
    * ✨ 토스 결제 요청 및 결제 검증 기능 추가
    
    * 🏷️ 토스 결제 요청 및 검증의 orderId 타입 수정

--- src/app/(with-main-header)/shop/RandomGatchaButton.tsx
+++ src/app/(with-main-header)/shop/RandomGatchaButton.tsx
@@ -5,21 +5,39 @@ import Modal from "@/components/common/Modal";
 import { useModalStore } from "@/store/modalStore";
 import Character from "@/components/common/Character";
 import basic from "@/assets/images/costumes/basic.png";
+import { shopApi } from "@/api/shop/shop";
+import { useState } from "react";
 
 export default function RandomGachaButton() {
   const { open, close } = useModalStore((state) => state);
+  const [costumeName, setCostumeName] = useState<string>("");
+  const [costumeImage, setCostumeImage] = useState<string>("");
+
+  const handlePurchaseRandomCostume = async (point = 100) => {
+    if (!point) return;
+    const purchaseCostume = await shopApi.postCostumeRandomPurchase(point);
+
+    if (purchaseCostume?.status === "성공") {
+      setCostumeName(purchaseCostume.data.costumeName);
+      setCostumeImage(purchaseCostume.data.image);
+      open("random-gacha");
+    }
+  };
 
   return (
     <>
-      <Button onClick={() => open("random-gacha")} className="bg-site-sub">
+      <Button
+        onClick={() => handlePurchaseRandomCostume(100)}
+        className="bg-site-sub"
+      >
         랜덤뽑기 100p
       </Button>
 
       <Modal modalId="random-gacha">
         <div className="flex flex-col items-center gap-8">
-          <Character costumeSrc={basic} />
+          <Character costumeSrc={costumeImage} />
           <div className="font-galmuri text-xl lg:text-2xl w-fit text-center">
-            <p>기본 코스튬에 당첨되었다</p>
+            <p>{costumeName}에 당첨되었다</p>
             <p>기쁘다!</p>
           </div>
         </div>
