14025b0d69f352e4a6494b737d9413418d45da8c
Soorangmanju <tina971007@naver.com>
2025-02-25 23:59:33 +0900

    💄 [Style] 퍼블리싱 추가-보드타입 드롭다운

--- src/app/(with-main-header)/boards/create/page.tsx
+++ src/app/(with-main-header)/boards/create/page.tsx
@@ -7,24 +7,67 @@ import AddPhotoAlternateRoundedIcon from "@mui/icons-material/AddPhotoAlternateR
 import Input from "@/components/common/Input";
 import ArrowDropDownRoundedIcon from "@mui/icons-material/ArrowDropDownRounded";
 import { FormEvent, useState } from "react";
+import Dropdown from "@/components/common/Dropdown";
 
 export default function BoardsCreate() {
   const [title, setTitle] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedBoardType, setSelectedBoardType] = useState("자유");
 
   const createPost = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log("게시글 작성");
   };
 
+  const handleTypeSelect = (type: string) => {
+    setSelectedBoardType(type);
+    setIsOpen(false);
+  };
+
   return (
     <form onSubmit={createPost} className="flex flex-col gap-6 px-5 lg:px-0">
-      <div className="flex justify-between items-center">
-        <Button type="button">
-          <span className="ml-2.5">자유</span>
+      <div className="relative flex justify-between items-center">
+        <Button
+          type="button"
+          onClick={(e) => {
+            e.preventDefault();
+            setIsOpen((prev) => !prev);
+          }}
+          className="flex items-center"
+        >
+          <span className="ml-2.5">{selectedBoardType}</span>
           <Icon MuiIcon={ArrowDropDownRoundedIcon} />
         </Button>
+
+        {isOpen && (
+          <Dropdown
+            onClose={() => setIsOpen(false)}
+            className="top-0 left-0 lg:mt-6"
+          >
+            <Button
+              type="button"
+              onClick={(e) => {
+                e.preventDefault();
+                handleTypeSelect("자유");
+              }}
+            >
+              자유
+            </Button>
+            <Button
+              type="button"
+              onClick={(e) => {
+                e.preventDefault();
+                handleTypeSelect("질문");
+              }}
+            >
+              질문
+            </Button>
+          </Dropdown>
+        )}
+
         <Button>작성하기</Button>
       </div>
+
       <div className="border-b border-site-darkgray-02">
         <Input
           value={title}
@@ -48,9 +91,9 @@ export default function BoardsCreate() {
           <input id="board-images" type="file" className="hidden" />
           <Icon MuiIcon={AddPhotoAlternateRoundedIcon} />
         </label>
-        {[1, 2].map((_, idx) => (
+        {/* {[1, 2].map((_, idx) => (
           <PostImage key={idx} />
-        ))}
+        ))} */}
       </div>
     </form>
   );
