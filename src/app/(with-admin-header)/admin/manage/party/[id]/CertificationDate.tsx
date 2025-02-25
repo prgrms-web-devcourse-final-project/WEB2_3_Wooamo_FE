31f2057c35a2035adc6f14885573a745bee6b9c8
ddongguri-bing <ckdalssnsk@naver.com>
2025-02-25 11:18:43 +0900

    ✨ 날짜 선택 컴포넌트에 partyId props 추가

--- src/app/(with-admin-header)/admin/manage/party/[id]/CertificationDate.tsx
+++ src/app/(with-admin-header)/admin/manage/party/[id]/CertificationDate.tsx
@@ -4,11 +4,12 @@ import { DatePicker } from "@/components/ui/datePicker";
 import { useState } from "react";
 
 interface DateProps {
+  partyId: number;
   start: string;
   end: string;
 }
 
-export default function CertificationDate({ start, end }: DateProps) {
+export default function CertificationDate({ partyId, start, end }: DateProps) {
   const startDate = new Date(start);
   const endDate = new Date(end);
   const [date, setDate] = useState<Date>(startDate);
