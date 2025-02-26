0243ceca0803ad36adf1952ed81af7735a5cb43d
ddongguri-bing <ckdalssnsk@naver.com>
2025-02-26 15:37:53 +0900

    🔥 불필요한 컴포넌트 파일 삭제

--- src/app/(with-main-header)/Timer.tsx
+++ src/app/(with-main-header)/Timer.tsx
@@ -6,14 +6,21 @@ import { timerApi } from "@/api/timer/timer";
 export default async function Timer() {
   const fetchTimerList = await timerApi.getTimerList();
   const timerList = fetchTimerList?.data;
+  const fetchStudyTimeForDaily = await timerApi.getStudyTimeForDaily();
+  const studyTimeForDaily = fetchStudyTimeForDaily?.data;
 
   if (!timerList) return;
+  if (!studyTimeForDaily) return;
 
   return (
     <section className="w-full lg:w-120 flex flex-col gap-4">
       <TimerCategory timerList={timerList} />
       <div className="px-3 py-3 bg-site-button rounded-lg">
-        <TimerPlayer />
+        <div className="flex justify-center items-center gap-7 mt-2 lg:mt-3 mb-5 lg:mb-6">
+          <p className="w-34 text-[32px] text-site-darkgray-02 font-galmuri">
+            {studyTimeForDaily.studyTime}
+          </p>
+        </div>
         <div className="flex flex-col gap-3">
           {timerList.map((timer) => (
             <TimerList
