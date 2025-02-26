cf55f64ed3501ce1f6e54010185554a4cfaa9574
dltjrrbs2020 <dltjrrbs2020@gmail.com>
2025-02-27 00:29:36 +0900

    🎨 다른 유저 프로필 페이지 - 월별 공부 시간, 로딩 스켈레톤 적용

--- src/app/(with-main-header)/users/[id]/page.tsx
+++ src/app/(with-main-header)/users/[id]/page.tsx
@@ -1,23 +1,33 @@
-"use client";
-
 import StudyTimeJandy from "./StudyTimeJandy";
 import PostsByUser from "./PostsByUser";
 import UserProfile from "./UserProfile";
-import { useParams } from "next/navigation";
-import { useIsMobile } from "@/hooks/useIsMobile";
 import Image from "next/image";
 import WhiteDividerShort from "@/assets/images/WhiteDividerShort.svg";
 
-export default function UserDetail() {
-  const { id } = useParams();
-  const isMobile = useIsMobile();
+interface UserDetailProps {
+  searchParams: Promise<{
+    year: number;
+    month: number;
+  }>;
+}
+
+export default async function UserDetail({ searchParams }: UserDetailProps) {
+  const { year: selectedYear, month: selectedMonth } = await searchParams;
+  const currentDate = new Date();
+
+  const year = selectedYear ?? currentDate.getFullYear();
+  const month = selectedMonth ?? currentDate.getMonth() + 1;
 
   return (
     <div className="flex flex-col lg:flex-row lg:justify-center items-center lg:items-start gap-12.5 lg:gap-20 px-5 lg:px-8">
       <UserProfile />
-      {isMobile && <Image src={WhiteDividerShort} alt="구분선 이미지" />}
+      <Image
+        src={WhiteDividerShort}
+        alt="구분선 이미지"
+        className="block lg:hidden"
+      />
       <section className="flex flex-col w-full lg:w-206 gap-7.5 lg:gap-13">
-        <StudyTimeJandy />
+        <StudyTimeJandy year={year} month={month} />
         <PostsByUser />
       </section>
     </div>
