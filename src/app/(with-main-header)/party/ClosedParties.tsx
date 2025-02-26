bedf3005f20e3b82029e6f30bc67307380b7fead
ddongguri-bing <ckdalssnsk@naver.com>
2025-02-26 23:53:21 +0900

    ✨ 마감된 팟 목록 조회 데이터 패칭

--- src/app/(with-main-header)/party/ClosedParties.tsx
+++ src/app/(with-main-header)/party/ClosedParties.tsx
@@ -1,5 +1,12 @@
+import { partyApi } from "@/api/party/party";
 import Button from "../../../components/common/Button";
-export default function ClosedParties() {
+
+export default async function ClosedParties() {
+  const fetchCompletedPartyList = await partyApi.getCompletedPartyList();
+  const completedPartyList = fetchCompletedPartyList?.data;
+
+  if (!completedPartyList) return;
+
   return (
     <section className="flex flex-col gap-7 mt-15">
       <p className="font-galmuri text-xl lg:text-2xl px-5 lg:px-0">
@@ -7,16 +14,26 @@ export default function ClosedParties() {
         <span className="ml-3">3</span>
       </p>
       <div className="flex flex-col gap-2.5 lg:gap-3">
-        {[1, 2, 3].map((_, index) => (
+        {completedPartyList.map((party) => (
           <article
-            key={index}
+            key={party.partyId}
             className="flex justify-between items-center h-14 lg:h-25 bg-site-button px-5"
           >
             <div className="flex gap-2.5 items-end">
-              <p className="font-semibold text-xl">팟 이름</p>
-              <p className="font-semibold">+500p</p>
+              <p className="font-semibold text-xl">{party.name}</p>
+              <p className="font-semibold">{party.rewordPoint}p</p>
             </div>
-            <Button className="bg-site-main text-white">보상받기</Button>
+            <Button
+              className={`${
+                party.questStatus === "보상받기"
+                  ? "bg-site-main text-site-white"
+                  : party.questStatus === "보상완료"
+                  ? "bg-site-profile text-site-black-100"
+                  : "bg-site-darkgray-01 text-white"
+              }`}
+            >
+              {party.questStatus}
+            </Button>
           </article>
         ))}
       </div>
