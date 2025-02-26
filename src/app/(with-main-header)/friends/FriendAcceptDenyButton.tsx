59cf75b87419822889b22b50e02da5240d35d0f8
dltjrrbs2020 <dltjrrbs2020@gmail.com>
2025-02-26 12:30:30 +0900

    ✨친구 요청 목록 및 친구 목록 로딩 스켈레톤 적용

---
+++ src/app/(with-main-header)/friends/FriendAcceptDenyButton.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { friendApi } from "@/api/friend/friend";
+import { revalidateTagAction } from "@/app/actions";
+import Button from "@/components/common/Button";
+
+export default function FriendAcceptDenyButton({
+  friend,
+}: {
+  friend: requestFriendType;
+}) {
+  const acceptFriend = async () => {
+    await friendApi.acceptFriend(friend.friendId);
+    revalidateTagAction("friends");
+  };
+
+  const deleteFriend = async () => {
+    await friendApi.deleteFriend(friend.friendId);
+    revalidateTagAction("request-friends");
+  };
+
+  return (
+    <div className="flex gap-2.5 lg:gap-5">
+      <Button onClick={acceptFriend} className="bg-site-main text-white">
+        수락
+      </Button>
+      <Button onClick={deleteFriend}>거절</Button>
+    </div>
+  );
+}
