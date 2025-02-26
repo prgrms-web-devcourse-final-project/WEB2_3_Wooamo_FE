b19b868e61c80b57a23bd409c5dc2aeeeeb28d07
dltjrrbs2020 <dltjrrbs2020@gmail.com>
2025-02-26 14:29:24 +0900

    🎨 마이페이지 - 스켈레톤 UI 적용을 위해, 서버 컴포넌트로 다수 리팩토링

--- src/app/(with-main-header)/mypage/ProfileTab.tsx
+++ src/app/(with-main-header)/mypage/ProfileTab.tsx
@@ -1,21 +1,20 @@
 import PostsByUser from "../users/[id]/PostsByUser";
 import StudyTimeJandy from "../users/[id]/StudyTimeJandy";
 import MyProfile from "./MyProfile";
-import { useIsMobile } from "@/hooks/useIsMobile";
 import Image from "next/image";
 import WhiteDividerShort from "@/assets/images/WhiteDividerShort.svg";
 
 export default function ProfileTab() {
-  const isMobile = useIsMobile();
-
   return (
     <div className="flex flex-col lg:flex-row lg:justify-center items-center lg:items-start gap-12.5 lg:gap-20 px-5 lg:px-8">
       <MyProfile />
-      {isMobile && (
-        <div className="block lg:hidden">
-          <Image src={WhiteDividerShort} alt="구분선 이미지" />
-        </div>
-      )}
+      <div>
+        <Image
+          src={WhiteDividerShort}
+          alt="구분선 이미지"
+          className="block lg:hidden"
+        />
+      </div>
       <section className="flex flex-col w-full lg:w-206 gap-7.5 lg:gap-13">
         <StudyTimeJandy />
         <PostsByUser />
--- src/app/(with-main-header)/mypage/page.tsx
+++ src/app/(with-main-header)/mypage/page.tsx
@@ -1,36 +1,41 @@
-"use client";
-
 import Link from "next/link";
 import Button from "../../../components/common/Button";
-import { useSearchParams } from "next/navigation";
 import { twMerge } from "tailwind-merge";
 import ProfileTab from "./ProfileTab";
 import ClosetTab from "./ClosetTab";
 
-export default function Mypage() {
-  const params = useSearchParams();
-  const tab = (params.get("tab") ?? "profile") as "profile" | "closet";
+export default async function Mypage({
+  searchParams,
+}: {
+  searchParams: Promise<{ tab: "profile" | "closet" }>;
+}) {
+  const { tab } = await searchParams;
+  const currentTab = tab ?? "profile";
   return (
     <div
       className={twMerge(
         "flex flex-col items-center gap-13",
-        tab === "closet" && "h-175 overflow-y-hidden",
+        currentTab === "closet" && "h-175 overflow-y-hidden",
       )}
     >
       <div className="flex items-center gap-3">
         <Link href={"/mypage?tab=profile"}>
-          <Button className={twMerge(tab !== "profile" && "bg-transparent")}>
+          <Button
+            className={twMerge(currentTab !== "profile" && "bg-transparent")}
+          >
             프로필
           </Button>
         </Link>
         <Link href={"/mypage?tab=closet"}>
-          <Button className={twMerge(tab !== "closet" && "bg-transparent")}>
+          <Button
+            className={twMerge(currentTab !== "closet" && "bg-transparent")}
+          >
             옷장
           </Button>
         </Link>
       </div>
-      {tab === "profile" && <ProfileTab />}
-      {tab === "closet" && <ClosetTab />}
+      {currentTab === "profile" && <ProfileTab />}
+      {currentTab === "closet" && <ClosetTab />}
     </div>
   );
 }
