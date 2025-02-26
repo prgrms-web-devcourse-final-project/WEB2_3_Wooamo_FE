ac9f1de410e7611c839bfda49c0b2ffbc8458418
dltjrrbs2020 <dltjrrbs2020@gmail.com>
2025-02-26 09:39:59 +0900

    ✨투두리스트 수정, 삭제 기능 리팩토링

--- src/app/(with-main-header)/TodoItems.tsx
+++ src/app/(with-main-header)/TodoItems.tsx
@@ -1,9 +1,7 @@
 import { todoApi } from "@/api/todo/todo";
 import TodoItem from "./TodoItem";
-import { delay } from "@/utils/delay";
 
 export default async function TodoItems() {
-  await delay(5000);
   const todos = await todoApi.getTodos();
   return (
     <div className="px-3 py-3 bg-site-button rounded-lg flex flex-col gap-3">
