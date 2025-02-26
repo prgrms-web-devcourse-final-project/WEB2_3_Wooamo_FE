ef25137cd01823d6f6a732665b9060fab4288dd0
ddongguri-bing <ckdalssnsk@naver.com>
2025-02-26 10:03:58 +0900

    ✨ 타이머 목록 데이터 패칭

--- src/app/(with-main-header)/Timer.tsx
+++ src/app/(with-main-header)/Timer.tsx
@@ -1,16 +1,29 @@
 import TimerList from "./TimerList";
 import TimerCategory from "./TimerCategory";
 import TimerPlayer from "./TimerPlayer";
+import { timerApi } from "@/api/timer/timer";
 
-export default function Timer() {
+export default async function Timer() {
+  const fetchTimerList = await timerApi.getTimerList();
+  const timerList = fetchTimerList?.data;
+
+  if (!timerList) return;
+
   return (
     <section className="w-full lg:w-120 flex flex-col gap-4">
       <TimerCategory />
       <div className="px-3 py-3 bg-site-button rounded-lg">
         <TimerPlayer />
         <div className="flex flex-col gap-3">
-          {new Array(5).fill(0).map((_, idx) => (
-            <TimerList key={idx} />
+          {timerList.map((timer) => (
+            <TimerList
+              key={timer.timerId}
+              timerId={timer.timerId}
+              categoryId={timer.categoryId}
+              name={timer.name}
+              studyDate={timer.studyDate}
+              studyTime={timer.studyTime}
+            />
           ))}
         </div>
       </div>
--- src/app/(with-main-header)/TimerList.tsx
+++ src/app/(with-main-header)/TimerList.tsx
@@ -2,14 +2,28 @@ import MoreHorizRoundedIcon from "@mui/icons-material/MoreHorizRounded";
 import Icon from "@/components/common/Icon";
 import Button from "@/components/common/Button";
 
-export default function TimerList() {
+interface TimerListProps {
+  timerId: number;
+  categoryId: number;
+  name: string;
+  studyDate: string;
+  studyTime: string;
+}
+
+export default function TimerList({
+  timerId,
+  categoryId,
+  name,
+  studyDate,
+  studyTime,
+}: TimerListProps) {
   return (
     <div className="flex justify-between px-2.5 lg:px-7 py-4 bg-site-white-100">
       <div className="flex gap-3 items-center">
-        <div className="text-xl">미적분 사랑해</div>
+        <div className="text-xl">{name}</div>
       </div>
       <div className="flex items-center gap-3">
-        <div className="text-xl">00:00:00</div>
+        <div className="text-xl">{studyTime}</div>
         <Button className="bg-transparent lg:px-0">
           <Icon MuiIcon={MoreHorizRoundedIcon} />
         </Button>
