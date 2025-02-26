23f3a2dc3a125e3a9e8d43a1fa0299cdb1c2f17c
dltjrrbs2020 <dltjrrbs2020@gmail.com>
2025-02-26 15:19:54 +0900

    🎨 마이페이지 - 내 작성글 조회 시, 로딩 스켈레톤 적용

--- src/app/(with-main-header)/users/[id]/PostsByUser.tsx
+++ src/app/(with-main-header)/users/[id]/PostsByUser.tsx
@@ -1,5 +1,7 @@
+import { Suspense } from "react";
 import PostItem from "../../boards/PostItem";
 import { userApi } from "@/api/user/user";
+import PostItemSkeleton from "@/components/common/skeletons/PostItemSkeleton";
 
 export default async function PostsByUser() {
   const posts = await userApi.getCurrentUserPosts();
@@ -9,12 +11,16 @@ export default async function PostsByUser() {
     <section className="flex flex-col gap-2 lg:gap-8">
       <p className="flex gap-1.5 font-semibold">
         <span>게시글</span>
-        <span>6</span>
+        <span>{posts.data.length}</span>
       </p>
       <div className="flex flex-col gap-5">
-        {posts.data.map((post) => (
-          <PostItem key={`post-${post.boardId}`} post={post} />
-        ))}
+        <div>
+          <Suspense fallback={<PostItemSkeleton count={3} />}>
+            {posts.data.map((post) => (
+              <PostItem key={`post-${post.boardId}`} post={post} />
+            ))}
+          </Suspense>
+        </div>
       </div>
     </section>
   );
