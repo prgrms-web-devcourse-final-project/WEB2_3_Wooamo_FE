97563938e6c6739f9fbbd78c82c1a85ec441e2fc
ddongguri-bing <ckdalssnsk@naver.com>
2025-02-27 15:44:56 +0900

    ✨ 개인 퀘스트 컴포넌트 분리

--- src/app/(with-main-header)/party/page.tsx
+++ src/app/(with-main-header)/party/page.tsx
@@ -3,26 +3,17 @@ import ClosedParties from "./ClosedParties";
 import EventParties from "./EventParties";
 import OngoingParties from "./OngoingParties";
 import UpcomingParties from "./UpcomingParties";
-import Button from "@/components/common/Button";
+import PersonalQuest from "./PersonalQuest";
 
 export default async function Party() {
   const fetchPersonalQuestState = await partyApi.getPersonalQuestState();
   const personalQuestState = fetchPersonalQuestState?.data.state;
 
-  console.log(personalQuestState);
+  if (!personalQuestState) return;
 
   return (
     <div className="flex flex-col relative">
-      <div className="fixed top-15 lg:top-25 left-0 w-screen h-15 bg-site-button flex justify-between items-center px-12 z-10">
-        <p className="font-semibold">[일일미션] 공부 시간 3시간 이상</p>
-        {personalQuestState === "보상받기" ? (
-          <Button className="h-0 lg:h-0 lg:text-base font-pretendard font-semibold">
-            {personalQuestState}
-          </Button>
-        ) : (
-          <p className="font-semibold">{personalQuestState}</p>
-        )}
-      </div>
+      <PersonalQuest personalQuestState={personalQuestState} />
       <EventParties />
       <UpcomingParties />
       <OngoingParties />
