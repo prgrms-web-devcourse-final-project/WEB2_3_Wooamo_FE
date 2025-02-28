5815e1eeace46469d1522d94f9b1190cea958c3e
ddongguri-bing <ckdalssnsk@naver.com>
2025-02-28 12:41:29 +0900

    ✨ 서버 accessToken 값 추가

--- src/app/(with-main-header)/page.tsx
+++ src/app/(with-main-header)/page.tsx
@@ -1,9 +1,15 @@
+import { cookies } from "next/headers";
 import AfterLoginHome from "./AfterLoginHome";
 import BeforeLoginHome from "./BeforeLoginHome";
 import { hasCookie } from "cookies-next";
 
 export default async function Home() {
   const isLoggedIn = hasCookie("accessToken");
+  const serverLoggedIn = (await cookies()).get("accessToken");
 
-  return isLoggedIn ? <AfterLoginHome /> : <BeforeLoginHome />;
+  return isLoggedIn || serverLoggedIn ? (
+    <AfterLoginHome />
+  ) : (
+    <BeforeLoginHome />
+  );
 }
