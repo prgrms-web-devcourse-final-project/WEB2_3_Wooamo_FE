7a13d1c36894e6c248a23f4e3e1b044c3a5a9559
dltjrrbs2020 <dltjrrbs2020@gmail.com>
2025-03-01 22:51:38 +0900

    ✨ 토스트 컴포넌트 모바일 반응형 추가

--- src/components/common/Toast.tsx
+++ src/components/common/Toast.tsx
@@ -16,7 +16,7 @@ interface ToastProps {
 const Toast = () => {
   const { toasts } = useToastStore();
   return (
-    <div className="fixed bottom-4 right-5 flex flex-col gap-2">
+    <div className="fixed bottom-4 right-1/2 translate-x-1/2 lg:right-5 lg:translate-0 flex flex-col gap-2">
       {toasts.map((toast, idx) => (
         <ToastItem key={toast.id} idx={idx} isAnimating={toast.isAnimating}>
           {toast.message}
@@ -30,7 +30,7 @@ const ToastItem = ({ children, isAnimating }: ToastProps) => {
   return (
     <div
       className={twMerge(
-        " flex justify-center items-center w-140 h-16.5 px-6 py-5 rounded-[12px] bg-site-white-100 transition-all shadow-xl animate-show-toast",
+        " flex justify-center items-center w-90 lg:w-fit h-18 lg:h-16.5 px-6 py-5 rounded-[12px] bg-site-white-100 transition-all shadow-xl animate-show-toast",
         isAnimating ? "translate-y-0" : "translate-y-20",
       )}
     >
@@ -38,7 +38,9 @@ const ToastItem = ({ children, isAnimating }: ToastProps) => {
         <Image src={PixelStarLeft} alt="픽셀화된 별 이미지" />
         <div className="flex gap-4">
           <Image src={Coin} alt="동전 이미지" />
-          <span className="text-xl font-galmuri">{children}</span>
+          <p className="flex flex-col lg:flex-row lg:text-xl font-galmuri break-keep">
+            {children}
+          </p>
         </div>
         <Image
           className="rotate-180"
