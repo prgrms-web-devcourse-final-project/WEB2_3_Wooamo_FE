227cee9f9a946bcb9cccaeaba4a531c3dd114aba
KimSoohyun <tina971007@naver.com>
2025-03-04 16:29:21 +0900

    [Feat]게시판 검색기능 구현 및 이미지 크게보기 (#106)
    * ✨ [FEAT] 실서버로 게시글 작성, 수정, 삭제, 댓글 작성, 삭제 구현현
    
    * 🐛 useRef 사용으로 변경경
    
    * ✨✨ [FEAT] 실시간 게시글 검색 구현
    
    * 🐛 검색 시 지연 기능 삭제 및 댓글 컴포넌트 채택 여부 수정
    
    * ✨ [FEAT] 게시글 상세 이미지 스와이퍼 적용
    
    * ✨ [FEAT] 이미지 크게보기 스와이퍼 적용

--- src/app/(with-main-header)/boards/BoardSearch.tsx
+++ src/app/(with-main-header)/boards/BoardSearch.tsx
@@ -1,38 +1,39 @@
 "use client";
 
 import InputIcon from "@/components/common/InputIcon";
-import useDebounce from "@/hooks/useDebounce";
 import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
 import { useRouter, useSearchParams } from "next/navigation";
-import { ChangeEvent, useEffect, useState } from "react";
+import { ChangeEvent, useRef, useState } from "react";
 
 export default function BoardSearch() {
   const router = useRouter();
   const searchParams = useSearchParams();
-  const searchValue = searchParams.get("title");
+  const prevKeyword = searchParams.get("title");
 
-  const [keyword, setKeyword] = useState(searchValue ?? "");
-  const debouncedValue = useDebounce(keyword, 200);
+  const timer = useRef<NodeJS.Timeout | null>(null);
+  const [keyword, setKeyword] = useState(prevKeyword ?? "");
 
   const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
     setKeyword(value);
+
+    if (timer.current) clearTimeout(timer.current);
+
+    timer.current = setTimeout(() => {
+      const trimmedValue = value.trim();
+
+      const params = new URLSearchParams();
+
+      if (trimmedValue) {
+        params.set("title", trimmedValue);
+      }
+      params.set("page", "0");
+
+      const url = `/boards?${params.toString()}`;
+      console.log("검색 URL:", url);
+      router.push(url);
+    }, 200);
   };
-
-  useEffect(() => {
-    if (debouncedValue) {
-      router.push(`/boards?title=${debouncedValue}`);
-    } else {
-      router.push("/boards");
-    }
-  }, [debouncedValue, router]);
-
-  useEffect(() => {
-    const searchValue = searchParams.get("title");
-    if (searchValue) {
-      setKeyword(searchValue);
-    }
-  }, [searchParams]);
 
   return (
     <InputIcon
@@ -40,6 +41,7 @@ export default function BoardSearch() {
       onChange={handleSearch}
       Icon={SearchRoundedIcon}
       placeholder="게시글 제목을 검색해주세요"
+      autoComplete="off"
     />
   );
 }
