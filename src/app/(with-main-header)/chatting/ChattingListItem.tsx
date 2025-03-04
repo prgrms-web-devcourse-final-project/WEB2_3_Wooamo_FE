db8736d0022f490885bd04fe133a5861255f252e
ddongguri-bing <ckdalssnsk@naver.com>
2025-03-04 09:33:57 +0900

    ✨ 채팅 목록 데이터 패칭 및 props 수정

--- src/app/(with-main-header)/chatting/ChattingListItem.tsx
+++ src/app/(with-main-header)/chatting/ChattingListItem.tsx
@@ -3,39 +3,52 @@ import basic from "@/assets/images/costumes/basic.png";
 import Link from "next/link";
 
 interface ChattingListItemProps {
-  id: number;
-  name: string;
-  lastChatContent: string;
-  read: boolean;
-  chatTimestamp: string;
+  roomId: string;
+  roomType: string;
+  roomName: string;
+  lastSenderId: number;
+  lastMessage: string;
+  profile: string;
+  unreadCount: number;
 }
 
 // 친구 채팅인지 팟 채팅인지 확인 후 라우팅 및 name 처리 해주시면 됩니다!
 export default function ChattingListItem({
-  id,
-  name,
-  lastChatContent,
-  read,
-  chatTimestamp,
+  roomId,
+  roomType,
+  roomName,
+  lastSenderId,
+  lastMessage,
+  profile,
+  unreadCount,
 }: ChattingListItemProps) {
   return (
-    <Link href={`/chatting/party/${id}`}>
+    <Link
+      href={
+        roomType === "PRIVATE"
+          ? `/chatting/friend/${roomId}`
+          : `chatting/party/${roomId}`
+      }
+    >
       <article
         className={`h-20 lg:h-25 px-5 lg:px-8 flex justify-between items-center ${
-          read ? "bg-site-white-50" : ""
+          unreadCount !== 0 ? "bg-site-white-50" : ""
         }`}
       >
         <div className="flex items-center gap-2.5">
           <div>
-            <Avatar className="w-11 lg:w-15 h-11 lg:h-15" costumeSrc={basic} />
+            <Avatar
+              className="w-11 lg:w-15 h-11 lg:h-15"
+              costumeSrc={profile}
+            />
           </div>
           <div className="flex flex-col gap-0 lg:gap-1">
             <div>
-              <span className="font-semibold text-xl">{name}</span>
+              <span className="font-semibold text-xl">{lastSenderId}</span>
             </div>
             <div className="flex gap-3 text-sm lg:text-base">
-              <span className="text-site-darkgray-02">{lastChatContent}</span>
-              <span className="text-site-darkgray-01">{chatTimestamp}</span>
+              <span className="text-site-darkgray-02">{lastMessage}</span>
+              <span className="text-site-darkgray-01">{}</span>
             </div>
           </div>
         </div>
--- src/app/(with-main-header)/chatting/page.tsx
+++ src/app/(with-main-header)/chatting/page.tsx
@@ -1,57 +1,40 @@
+import { chattingApi } from "@/api/chatting/chatting";
 import ChattingHeader from "./ChattingHeader";
 import ChattingListItem from "./ChattingListItem";
+import { userApi } from "@/api/user/user";
 
-export default function Chatting() {
+export default async function Chatting() {
+  const fetchCurrentUser = await userApi.getCurrentUserInfo();
+  const userId = fetchCurrentUser?.data.userId;
+  if (!userId) return;
+
+  const fetchChattingList = await chattingApi.getChattingList(userId);
+  const chattingList = fetchChattingList?.data;
+
+  console.log(fetchChattingList?.message);
+
   return (
     <div className="flex flex-col gap-13">
       <ChattingHeader />
       <section className="flex flex-col">
-        {new Array(5)
-          .fill(0)
-          .map((_, idx) =>
-            idx % 2 === 0 ? (
-              <ChattingListItem
-                key={idx}
-                id={idx}
-                name={`@user ${idx}`}
-                lastChatContent="혹시 자료 공유 해주실 수 있나요?"
-                read={true}
-                chatTimestamp="방금"
-              />
-            ) : (
-              <ChattingListItem
-                key={idx}
-                id={idx}
-                name={`@user ${idx}`}
-                lastChatContent="반가워요!"
-                read={false}
-                chatTimestamp="3시간 전"
-              />
-            )
-          )}
-        {new Array(5)
-          .fill(0)
-          .map((_, idx) =>
-            idx % 2 === 0 ? (
-              <ChattingListItem
-                key={idx}
-                id={idx}
-                name={`party ${idx}`}
-                lastChatContent="혹시 자료 공유 해주실 수 있나요?"
-                read={true}
-                chatTimestamp="방금"
-              />
-            ) : (
-              <ChattingListItem
-                key={idx}
-                id={idx}
-                name={`party ${idx}`}
-                lastChatContent="반가워요!"
-                read={false}
-                chatTimestamp="3시간 전"
-              />
-            )
-          )}
+        {chattingList ? (
+          chattingList.map((chatting) => (
+            <ChattingListItem
+              roomId={chatting.roomId}
+              roomType={chatting.roomType}
+              roomName={chatting.roomName}
+              lastSenderId={chatting.lastSenderId}
+              lastMessage={chatting.lastMessage}
+              profile={chatting.profile}
+              unreadCount={chatting.unreadCount}
+            />
+          ))
+        ) : (
+          <div className="text-site-darkgray-02">
+            <p>채팅 목록이 없습니다</p>
+            <p>다른 유저와 채팅을 시작해보세요!</p>
+          </div>
+        )}
       </section>
     </div>
   );
