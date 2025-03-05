61fa5b4de1553c86deccfb6d8dcc867c58c739d6
ddongguri-bing <ckdalssnsk@naver.com>
2025-03-05 23:49:41 +0900

    ✨ 관리자 팟 인증 상세 페이지 컴포넌트 분리

---
+++ src/app/(with-admin-header)/admin/manage/party/[id]/Certification.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { useParams } from "next/navigation";
+import CertificationDate from "./CertificationDate";
+import CertificationViewer from "./CertificationViewer";
+import ParticipantItem from "./ParticipantItem";
+import { useState } from "react";
+
+export default function Certification({
+  partyDetail,
+}: {
+  partyDetail: PartyDetailDataType;
+}) {
+  const { id } = useParams();
+  const partyMembers = partyDetail.members;
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [selectedMember, setSelectedMember] = useState<PartyMemberType>(
+    partyMembers[0],
+  );
+
+  console.log(selectedDate);
+  console.log(selectedMember);
+
+  const handleDateSelect = (date: Date) => {
+    setSelectedDate(date);
+  };
+
+  const handleUserSelect = (member: PartyMemberType) => {
+    setSelectedMember(member);
+  };
+
+  return (
+    <>
+      <div className="flex flex-col gap-5">
+        <div className="font-semibold text-xl">날짜 선택</div>
+        <CertificationDate
+          partyId={Number(id)}
+          start={partyDetail.startDate}
+          end={partyDetail.endDate}
+          onChange={handleDateSelect}
+          selectedDate={selectedDate}
+        />
+      </div>
+      <div className="flex gap-30">
+        <CertificationViewer
+          selectedDate={selectedDate}
+          selectedMember={selectedMember}
+        />
+        <div className="flex flex-col gap-5">
+          <div className="font-semibold text-xl">팟 인원</div>
+          <div className="flex flex-wrap gap-8">
+            {partyMembers.map((member) => (
+              <ParticipantItem
+                key={member.memberId}
+                member={member}
+                onChange={handleUserSelect}
+                selectedMember={selectedMember}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
