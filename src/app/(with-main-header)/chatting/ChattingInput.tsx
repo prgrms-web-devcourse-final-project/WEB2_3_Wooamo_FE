d8870401335e6b0da7c96ffa765c1ef682699b0b
dltjrrbs2020 <dltjrrbs2020@gmail.com>
2025-03-05 23:09:17 +0900

    ✨ 채팅 - 채팅 보내기 및 조회

--- src/app/(with-main-header)/chatting/ChattingInput.tsx
+++ src/app/(with-main-header)/chatting/ChattingInput.tsx
@@ -1,20 +1,50 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { FormEvent, useState } from "react";
 import InputIcon from "@/components/common/InputIcon";
 import SendRoundedIcon from "@mui/icons-material/SendRounded";
+import { useSocketStore } from "@/store/socketStore";
 
-export default function ChattingInput() {
+interface ChattingInputProps {
+  currentUser: userType;
+  roomId: string;
+}
+
+export default function ChattingInput({
+  currentUser,
+  roomId,
+}: ChattingInputProps) {
+  const { disconnect, sendMessage } = useSocketStore();
   const [chatting, setChatting] = useState("");
+
+  const sendChatting = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const trimmedChatting = chatting.trim();
+    if (trimmedChatting === "") return;
+
+    console.log("currentUser: ", currentUser);
+    sendMessage({
+      roomId,
+      userInfo: {
+        userId: currentUser.userId,
+        nickname: currentUser.nickname,
+        profile: currentUser.profile ?? "",
+      },
+      message: trimmedChatting,
+    });
+    setChatting("");
+    return () => disconnect();
+  };
+
   return (
-    <form action="" className="fixed bottom-5 left-0 w-full px-5 lg:px-0">
+    <form
+      onSubmit={sendChatting}
+      className="fixed bottom-5 left-0 w-full px-5 lg:px-0"
+    >
       <InputIcon
         value={chatting}
         Icon={SendRoundedIcon}
         onChange={(e) => setChatting(e.target.value)}
-        onClickIcon={function (): void {
-          throw new Error("Function not implemented.");
-        }}
         placeholder="채팅을 입력해주세요"
       />
     </form>
--- src/app/(with-main-header)/chatting/ChattingItem.tsx
+++ src/app/(with-main-header)/chatting/ChattingItem.tsx
@@ -1,8 +1,12 @@
+import Avatar from "../../../components/common/Avatar";
+
 interface ChattingItemProps {
   chatTime: string;
   nickname: string;
   isMe: boolean;
   chatting: string;
+  profile: string | null;
+  unreadCount: number;
 }
 
 export default function ChattingItem({
@@ -10,13 +14,16 @@ export default function ChattingItem({
   nickname,
   isMe,
   chatting,
+  profile,
+  unreadCount,
 }: ChattingItemProps) {
   return (
     <>
       {isMe ? (
         <div className="flex justify-end my-5">
-          <div className="flex flex-col justify-end mr-2 lg:mr-3">
-            <p className="text-xs text-site-darkgray-02">{chatTime}</p>
+          <div className="flex flex-col justify-end mr-2 lg:mr-3 text-site-darkgray-02">
+            <p className="text-sm">{unreadCount}</p>
+            <p className="text-xs">{chatTime}</p>
           </div>
           <div className="flex items-start gap-3 lg:gap-5">
             <div className="px-5 lg:px-7 py-4 bg-site-profile rounded-xl lg:max-w-190">
@@ -25,7 +32,10 @@ export default function ChattingItem({
               </span>
             </div>
             <div className="flex">
-              <div className="h-11 lg:h-14 w-11 lg:w-14 bg-site-darkgray-01 rounded-full"></div>
+              <Avatar
+                className="h-11 lg:h-14 w-11 lg:w-14"
+                costumeSrc={profile ?? ""}
+              />
             </div>
           </div>
         </div>
@@ -33,7 +43,10 @@ export default function ChattingItem({
         <div className="flex my-5">
           <div className="flex items-start gap-3 lg:gap-5">
             <div className="flex">
-              <div className="h-11 lg:h-14 w-11 lg:w-14 bg-site-darkgray-01 rounded-full"></div>
+              <Avatar
+                className="h-11 lg:h-14 w-11 lg:w-14"
+                costumeSrc={profile ?? ""}
+              />
             </div>
             <div className="px-5 lg:px-7 py-4 bg-site-white-100 rounded-xl lg:max-w-190">
               <span className="text-sm lg:text-base text-black">
@@ -41,8 +54,9 @@ export default function ChattingItem({
               </span>
             </div>
           </div>
-          <div className="flex flex-col justify-end ml-2 lg:ml-3">
-            <p className="text-xs text-site-darkgray-02">{chatTime}</p>
+          <div className="flex flex-col justify-end ml-2 lg:ml-3 text-site-darkgray-02">
+            <p className="text-sm">{unreadCount}</p>
+            <p className="text-xs">{chatTime}</p>
           </div>
         </div>
       )}
