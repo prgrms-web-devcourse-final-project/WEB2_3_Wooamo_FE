5a2f6db2c7c41f024c8ffd6274c520f0ac383800
KimSoohyun <tina971007@naver.com>
2025-03-06 16:50:01 +0900

    [Fix] 게시판 페이지 버그 수정 (#125)
    * 🐛 [FIX] 게시글 이미지 모달 키보드 escape 적용
    
    * 🐛 [FIX] BR 적용과costumeSrc 오류 수정
    
    * [FIX] 줄바꿈 추가
    
    * 🐛 [FIX] 미로그인사용자 로그인페이지 이동
    
    * 🐛 [FIX] useClient  사용문제 수정

--- src/app/(with-main-header)/boards/BoardsHeader.tsx
+++ src/app/(with-main-header)/boards/BoardsHeader.tsx
@@ -1,15 +1,15 @@
-"use client";
-
 import Button from "@/components/common/Button";
 import Link from "next/link";
 import BoardSearch from "./BoardSearch";
+import { userApi } from "@/api/user/user";
 
-export default function BoardsHeader() {
+export default async function BoardsHeader() {
+  const userInfo = await userApi.getCurrentUserInfo();
   return (
     <div className="flex flex-col gap-4 lg:gap-7">
       <div className="flex items-center justify-between">
         <p className="font-galmuri text-xl lg:text-[28px]">전체</p>
-        <Link href={"boards/create"}>
+        <Link href={userInfo?.data ? "/boards/create" : "/signin"}>
           <Button type="button">글쓰기</Button>
         </Link>
       </div>
