b57d33324763c62e6eeb08802ecee9c5339d081f
dltjrrbs2020 <dltjrrbs2020@gmail.com>
2025-03-07 12:06:08 +0900

    🔥 스켈레톤 확인용 delay 함수 제거

--- src/app/(with-main-header)/friends/FriendsItem.tsx
+++ src/app/(with-main-header)/friends/FriendsItem.tsx
@@ -1,9 +1,7 @@
 import ProfileSummary from "@/components/common/ProfileSummary";
-import { delay } from "@/utils/delay";
 import FriendDeleteButton from "./FriendDeleteButton";
 
 export default async function FriendsItem({ friend }: { friend: friendType }) {
-  await delay(5000);
   return (
     <article className="h-19 lg:h-25 flex justify-between items-center">
       <ProfileSummary
