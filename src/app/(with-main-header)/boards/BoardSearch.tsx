e908bb31b5a96e475f60d1be715188ad4187c410
ddongguri-bing <ckdalssnsk@naver.com>
2025-03-09 10:29:43 +0900

    ✨ 게시판 페이지네이션 적용

--- src/app/(with-main-header)/boards/BoardSearch.tsx
+++ src/app/(with-main-header)/boards/BoardSearch.tsx
@@ -10,29 +10,18 @@ export default function BoardSearch() {
   const searchParams = useSearchParams();
   const prevKeyword = searchParams.get("title");
 
-  const timer = useRef<NodeJS.Timeout | null>(null);
   const [keyword, setKeyword] = useState(prevKeyword ?? "");
 
   const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
     setKeyword(value);
 
-    if (timer.current) clearTimeout(timer.current);
-
-    timer.current = setTimeout(() => {
-      const trimmedValue = value.trim();
-
-      const params = new URLSearchParams();
-
-      if (trimmedValue) {
-        params.set("title", trimmedValue);
-      }
-      params.set("page", "0");
-
-      const url = `/boards?${params.toString()}`;
-      console.log("검색 URL:", url);
-      router.push(url);
-    }, 200);
+    const trimmedValue = value.trim();
+    if (trimmedValue) {
+      router.push(`?title=${trimmedValue}`);
+    } else {
+      router.push(`/`);
+    }
   };
 
   return (
--- src/app/(with-main-header)/boards/page.tsx
+++ src/app/(with-main-header)/boards/page.tsx
@@ -1,40 +1,23 @@
-import PostItem from "./PostItem";
 import BoardsHeader from "./BoardsHeader";
-import { boardApi } from "@/api/board/board";
 import { Suspense } from "react";
 import PostItemSkeleton from "@/components/common/skeletons/PostItemSkeleton";
+import PostItems from "./PostItems";
 
 interface PageProps {
-  searchParams: Promise<{
-    title?: string;
-    page?: string;
-  }>;
+  searchParams: Promise<{ title: string; page: string }>;
 }
 
 export default async function Boards({ searchParams }: PageProps) {
-  const params = await searchParams;
-  const title = params?.title?.trim() || "";
-  const page = Math.max(0, Number(params?.page || "0"));
+  const { title, page } = await searchParams;
 
-  try {
-    const posts = await boardApi.getBoardList(page, title);
-
-    if (!posts?.data?.contents) return null;
-
-    return (
-      <div className="flex flex-col gap-10 lg:gap-13 px-5 lg:px-0">
-        <BoardsHeader />
-        <section className="flex flex-col gap-2 lg:gap-5">
-          <Suspense fallback={<PostItemSkeleton count={3} />}>
-            {posts.data.contents.map((post) => (
-              <PostItem key={post.boardId} post={post} />
-            ))}
-          </Suspense>
-        </section>
-      </div>
-    );
-  } catch (error) {
-    console.error("Error loading boards:", error);
-    return <div>게시글을 불러오는데 실패했습니다.</div>;
-  }
+  return (
+    <div className="flex flex-col gap-10 lg:gap-13 px-5 lg:px-0">
+      <BoardsHeader />
+      <section className="flex flex-col gap-2 lg:gap-5">
+        <Suspense fallback={<PostItemSkeleton count={3} />}>
+          <PostItems title={title} page={page} />
+        </Suspense>
+      </section>
+    </div>
+  );
 }
