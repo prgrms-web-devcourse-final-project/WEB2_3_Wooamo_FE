5541f0b6cf2c1253010cdfd67bd4d8e98d6de285
dltjrrbs2020 <dltjrrbs2020@gmail.com>
2025-03-09 19:45:59 +0900

    ✨  비로그인 사용자가 URL을 쳐서 들어가는 경우, 로그인 페이지로 이동하게 설정

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,26 @@
 import { NextRequest, NextResponse } from "next/server";
+import { userApi } from "./api/user/user";
 
 export async function middleware(request: NextRequest) {
-  // console.log(request);
+  const { pathname } = request.nextUrl;
+  if (pathname === "/") {
+    return NextResponse.next();
+  }
 
+  const isLoggedIn = await userApi.checkIsLoggedIn();
+  if (!isLoggedIn?.data) {
+    return NextResponse.redirect(`${request.nextUrl.origin}/signin`);
+  }
   return NextResponse.next();
 }
+
+export const config = {
+  matcher: [
+    "/mypage",
+    "/friends",
+    "/boards/create",
+    "/(party/(?!all$).*)",
+    "/chatting/:path",
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|signin|signup|shop|party|boards|users).*)",
+  ],
+};
