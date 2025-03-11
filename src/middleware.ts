b26af33afe4bea1df545a510eb56858cb32d8564
dltjrrbs2020 <dltjrrbs2020@gmail.com>
2025-03-12 00:19:51 +0900

    🐛 미들웨어 - 디버깅

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,11 +3,14 @@ import { userApi } from "./api/user/user";
 
 export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
+  console.log("request.nextUrl: ", request.nextUrl);
+  console.log("pathname: ", pathname);
   if (pathname === "/") {
     return NextResponse.next();
   }
 
   const isLoggedIn = await userApi.checkIsLoggedIn();
+  console.log("isLoggedIn: ", isLoggedIn);
   if (!isLoggedIn?.data) {
     return NextResponse.redirect(`${request.nextUrl.origin}`);
   }
@@ -23,6 +26,7 @@ export const config = {
     "/chatting/:path",
     "/friends/add",
     "/friends",
+    "/admin/:path",
     "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|signin|signup|shop|party|boards|users|friends|chatting).*)",
   ],
 };
