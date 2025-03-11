2e250c595894d6cf09e4ee5f18927a75950593b8
dltjrrbs2020 <dltjrrbs2020@gmail.com>
2025-03-11 23:07:57 +0900

    ✨ 하드코딩된 링크를 환경변수로 변경

--- src/app/(with-auth-header)/signin/page.tsx
+++ src/app/(with-auth-header)/signin/page.tsx
@@ -136,7 +136,7 @@ export default function SignIn() {
         <Image src={Divider} alt="구분선" className="" />
       </div>
       <Link
-        href={`https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_KEY}&redirect_uri=https://43.202.98.4.nip.io/api/kakaoLogin`}
+        href={`https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_CLIENT_URL}/api/kakaoLogin`}
         className="lg:w-150"
       >
         {isMobile ? (
